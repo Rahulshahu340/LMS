45a378701efd5a97178daaf52bc98a627ac11c8d
Rahulshahu340 <rahuldbc340@gmail.com>
2023-09-09 12:53:35 +0530

    uogdate first commit

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Building the project"
-python3.9 pip install  -r requirement.txt
+python3.11.5 pip install  -r requirement.txt
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.11.5 manage.py makemigrations --noinput
+python3.11.5 manage.py migrate --noinput
 
 echo 'collect static...'
-python3.9 manage.py collect --noinput --clear
+python3.11.5 manage.py collect --noinput --clear
