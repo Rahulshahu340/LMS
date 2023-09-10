bd8dce4402fc42d62bb2425fd8cd1354db82f46e
Rahulshahu340 <rahuldbc340@gmail.com>
2023-09-10 08:18:26 +0530

    updated final commit

--- LMS/urls.py
+++ LMS/urls.py
@@ -61,6 +61,6 @@ urlpatterns = [
     
     path('course/watch-course/<slug:slug>', views.WATCH_COURSE,name='watch_course'),
 
-]
+]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
